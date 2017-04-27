6a9716c996a4765cae7eaf37b5dd3127c5af8829
Knut Hühne <knut@k-nut.eu>
2017-04-27 08:46:02 +0200

    update url in sample settings to point to new location

--- settings.sample.py
+++ settings.sample.py
@@ -3,10 +3,10 @@ SERVICES = {
     'anlagenbaeume': {
         'filename': 'anlagenbaeume.xml',
         'tmp_dir': '/tmp/',
-        'url': 'http://fbinter.stadt-berlin.de/fb/wfs/geometry/senstadt/re_anlagenbaeume',
+        'url': 'http://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/s_wfs_baumbestand',
         'extend': [367190.96, 5798934.34, 417447.14, 5838875.02],
         'srid': 'EPSG:25833',
-        'layer': 'fis:re_anlagenbaeume',
+        'layer': 'fis:s_wfs_baumbestand',
         'paging': {
             'nx': 4,
             'ny': 4
@@ -15,10 +15,10 @@ SERVICES = {
     'strassenbaeume': {
         'filename': 'strassenbaeume.xml',
         'tmp_dir': '/tmp/',
-        'url': 'http://fbinter.stadt-berlin.de/fb/wfs/geometry/senstadt/re_strassenbaeume',
+        'url': 'http://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/s_wfs_baumbestand_an',
         'extend': [367190.96, 5798934.34, 417447.14, 5838875.02],
         'srid': 'EPSG:25833',
-        'layer': 'fis:re_strassenbaeume',
+        'layer': 'fis:s_wfs_baumbestand_an',
         'paging': {
             'nx': 4,
             'ny': 4
